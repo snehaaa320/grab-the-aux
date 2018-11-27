60777d04d22a88dce4b6d2744f1f8a0af2bc7a10
Nish Ullagaddi <nishbasu@gmail.com>
2018-11-26 18:19:44 -0800

    updated choose_playlist.js
    - added images functionality
    - changed to 6 
    - updated token

--- choose_playlist.js
+++ choose_playlist.js
@@ -1,13 +1,11 @@
-var token = 'BQDI8BFThLtd0TnEKaZHV3E2z4gGMx_dXEu3sdwoEgELKKJwayCn_551f8Q-iHlbWkbtf1U_2iGkaAAtoZayjNDnfSBcNv0OvVmoOYB89VU0EUvUBiZSOg5eBP9yD8iTU5YE8CDLOD7YtsC5nionIwn9V-Rt7wZ-7KP9JWlUdhHEuYm-QEckAOF1dvBx3H4yXXXZtdvua5W5MSc3jsd1Yx-4Qr3t-y9DgWW5op6l0qZBAQubBUGiuGfj1Ld_aY_iCh2-6HDCqiZ_0CjsoGa7493avQDAGBPFPrLCCtwo';
+var token = 'BQBuB9HhhduAtcBJtUBj-W3dsKGkiEAU3g8sf8OIAKXuwUOiIIDmSYZvkV5N_OXZTFXKGgIo5fvJAwsDr1P7m9ni0WoCz0TpN1rEpyYrQ6WUAnNHr-Sf-14BMnWICJxvTFBnjc1J4lQ1UgHOnQIrM9AxxMDonLjbOpK7g1pTmIdD7VMQlP6BVpx80QFK3UFN3HouzhNyOm0DwuqRTOcV2DsXo41SyDrIPRmlmk_WA9Iim3dC82zU_3Bt-fsqNu1dvB0Wmmv9x1XMukT8Lj2XAHDr0O3ofEs_HUhpbAY';
 var playlistid = '2ysn3h0Vmkb4sqmCDyCgqO';
 var user_uri ='22wzuycdg3qep6pwszio7pizi';
-//var token = get_access_token();
-//var user_uri = get_user_uri();
-
-var song = [];
+var song = []
 var playlists = [];
 var map = {};
 var selected_playlist;
+var images = [];
 
 function getUserPlaylists() {
 
@@ -17,19 +15,31 @@ function getUserPlaylists() {
     spotifyApi.getUserPlaylists() // note that we don't pass a user id
         .then(function(data) {
             console.log('User playlists', data);
+
+
             console.log(data["items"][0]["name"]);
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < 6; i++) {
                 //playlists stores the uri of each playlist
                 playlists.push(data["items"][i]);
+
+                images.push(data["items"][i]["images"][0]["url"]);
 
                 //this is a map of the uri of the playlist to the name of the playlist
                 map[playlists[i]] = data["items"][i]["name"];
 
                 var p = "playlist" + i;
+                var image="image" + i;
+
                 document.getElementById(p).innerHTML = map[playlists[i]];
+                document.getElementById(image).innerHTML = '<img width="200" height="200" src='+ images[i]+'>';
+
                 console.log(playlists[i]);
+                console.log(images[i]);
                 console.log;
             }
+
+            console.log(images[0]);
+
         }, function(err) {
             console.error(err);
         });
@@ -59,7 +69,8 @@ function create() {
         }, function(err) {
             console.error(err);
         });
-    redirect_to_user_view(selected_playlist)
+    alert("Your playlist has been created. Please refresh your page!");
+    redirect_to_user_view(selected_playlist);
     return selected_playlist;
 }
 
@@ -68,3 +79,7 @@ function redirect_to_user_view(selected_playlist) {
     console.log("Playlist ID = " + selected_playlist["id"]);
     window.location.href = "user_view.html?is_guest=false&owner_id="+selected_playlist["owner"]["id"]+ "&playlist_id=" + selected_playlist["id"];
 }
+
+
+
+
