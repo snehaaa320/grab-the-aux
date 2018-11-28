af65bb3f2a97b3afc12bf71bc3dde2a6587685f3
Jusman Singh <singh.jusman@yahoo.com>
2018-11-27 18:25:04 -0800

    ordering by upvotes

--- user_view.js
+++ user_view.js
@@ -102,12 +102,12 @@ firebase.database().ref("/").child('.info/connected').on('value', function(conne
 function getValuesFromDB() {
     if (playlist_token !== null) {
         console.log('Playlist_Table/' + playlist_token + '/Queue/')
-        var queueRef = firebase.database().ref('Playlist_Table/' + playlist_token + '/Queue/').orderByChild('queue_num');
+        var queueRef = firebase.database().ref('Playlist_Table/' + playlist_token + '/Queue/').orderByChild('Upvotes');
         queueRef.on('value', function(queue) {
             num_of_songs = queue.numChildren() + 1;
             console.log(num_of_songs);
             queue.forEach(function(song) {
-                printToTable(song.child('queue_num').val(), song.key, song.child('Artist').val(), song.child('Upvotes').val());
+                printToTable(song.key, song.child('Artist').val(), song.child('Upvotes').val());
             });
         }, function(error) {
             console.log("Error : " + error.code);
@@ -115,26 +115,24 @@ function getValuesFromDB() {
     }
 }
 
-function printToTable(queue_num, song_name, song_artist, currentVote) {
+function printToTable(song_name, song_artist, currentVote) {
     console.log(song_name + ' ' + song_artist + ' ' + currentVote);
     var table = document.getElementById('song_table');
     if (table.rows.length == num_of_songs) {
         table.deleteRow(1);
     }
-    var new_row = table.insertRow(-1);
-    var queue_cell = new_row.insertCell(0);
-    var song_name_cell = new_row.insertCell(1);
-    var song_artist_cell = new_row.insertCell(2);
-    var upvotes = new_row.insertCell(3);
+    var new_row = table.insertRow(1);
+    var song_name_cell = new_row.insertCell(0);
+    var song_artist_cell = new_row.insertCell(1);
+    var upvotes = new_row.insertCell(2);
 
-    queue_cell.innerHTML = queue_num;
     song_name_cell.innerHTML = song_name;
     song_artist_cell.innerHTML = song_artist;
     upvotes.innerHTML = '<img src="upvote.png" alt="Up Vote" height="20px" onclick="update_vote_count(\'' + song_name + '\',' + (currentVote + 1) + ', 1)"/> &nbsp;' + currentVote;
     upvotes.innerHTML += '&nbsp;&nbsp;<img src="downvote.png" alt="Down Vote" height="20px" onclick="update_vote_count(\'' + song_name + '\',' + (currentVote - 1) + ', 0)" />';
     if (!is_guest) {
         var played_button = new_row.insertCell(4);
-        played_button.innerHTML = '<button type="submit" name="song_played" onclick="removeSong(' + queue_num + ',\'' + song_name + '\',\'' + song_artist + '\');">Yes</button>';
+        played_button.innerHTML = '<button type="submit" name="song_played" onclick="removeSong(\'' + song_name + '\',\'' + song_artist + '\');">Yes</button>';
     }
 }
 
@@ -146,7 +144,6 @@ function writeToDB() {
     firebase.database().ref('Playlist_Table/' + playlist_token + '/Queue/' + song_name).set({
         Artist: song_artist,
         Upvotes: 0,
-        queue_num: num_of_songs,
     });
 
 }
@@ -181,8 +178,8 @@ function update_vote_count(song_name, new_vote, upvote) {
     return firebase.database().ref().update(update);
 }
 
-function removeSong(queue_num, song_name, song_artist) {
-    console.log(queue_num + "\n" + song_name + "\n" + song_artist);
+function removeSong(song_name, song_artist) {
+    console.log(song_name + "\n" + song_artist);
     var update = {};
 
     var temp_queue_num = 1;
@@ -192,8 +189,8 @@ function removeSong(queue_num, song_name, song_artist) {
     while (table.rows.length > 1) {
         console.log(table.rows.length);
         console.log("Inside while loop");
-        if (table.rows[1].cells[0].innerHTML != queue_num) {
-            update['Playlist_Table/' + playlist_token + '/Queue/' + table.rows[1].cells[1].innerHTML + '/queue_num'] = temp_queue_num++;
+        if (table.rows[1].cells[0].innerHTML != song_name) {
+            // update['Playlist_Table/' + playlist_token + '/Queue/' + table.rows[1].cells[1].innerHTML + '/queue_num'] = temp_queue_num++;
         }
         table.deleteRow(1);
     }
@@ -238,7 +235,7 @@ function parseURL() {
 function hideHostFunctions() {
     document.getElementById("search_bar").style.display = "none";
     var song_table = document.getElementById("song_table");
-    song_table.rows[0].deleteCell(4);
+    song_table.rows[0].deleteCell(3);
 }
 
 function generateUniqueKey() {
@@ -279,7 +276,7 @@ function createNewToken() {
     firebase.database().ref('Playlist_Table/' + playlist_token).set({
         spotify_playlist_id: playlist_id,
         spotify_user_uri: user_uri,
-        host_name : user,
+        host_name: user,
     });
     // document.getElementById("playlist_token").innerHTML += playlist_token;
     console.log("DB - Written " + playlist_token);
@@ -302,10 +299,9 @@ function getPlaylist(playlist_token) {
 }
 
 function returnToHome() {
-    if(is_guest){
+    if (is_guest) {
         window.location.href = "user_login.html";
-    }
-    else{
+    } else {
         window.location.href = "choose_playlist.html";
     }
 }
